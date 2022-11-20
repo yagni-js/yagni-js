d43f5b5f2fdc7cbba1c1b7a65fcdaedfca6edbe9
Yuri Egorov <ysegorov@gmail.com>
2022-11-20 18:00:17 +0000

    [yagni-loader] update rollup.config.js

--- packages/yagni-loader/rollup.config.js
+++ packages/yagni-loader/rollup.config.js
@@ -1,12 +1,12 @@
 
 import pkg from './package.json';
-import { eslint } from 'rollup-plugin-eslint';
+import eslint from '@rollup/plugin-eslint';
 
 export default [
   {
     input: 'src/main.js',
     output: [
-      {file: pkg.main, format: 'cjs'},
+      {file: pkg.main, format: 'cjs', exports: 'named'},
       {file: pkg.module, format: 'es'}
     ],
     external: [
